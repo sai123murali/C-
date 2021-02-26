fb204388276a8437ba3b57db9980bd54a5723b8a
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2021-02-26 20:59:00 +0530

    Create strcmp.cpp

---
+++ strcmp.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    
+    char str[16][10]={"break", "case", "continue", "default", "defer", "else","for", 
+    "func", "goto", "if", "map", "range", "return", "struct", "type", "var"};
+    
+    char input[20];
+    
+    int flag=0,i=0;
+    
+    cin>>input;
+    
+    for(i=0;i<16;i++)
+    {
+        if(strcmp(input,str[i])==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+    
+    if(flag==1)
+    {
+        cout<<input<<"is a keyword";
+    }
+    else
+    {
+        cout<<input<<"is not a keyword";
+    }
+}
