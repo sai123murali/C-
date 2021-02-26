2b7bbcc223cdb2d46f10250f75dcce651a6565f6
Epuru Sai Muralidhar <32213467+sai123murali@users.noreply.github.com>
2021-02-26 21:18:08 +0530

    Create string change.cpp

---
+++ string change.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+    
+    char a[100],b[100],c[100];
+    int i=0;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    
+    for( i=0;a[i]!='\0';i++)
+    {
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+            a[i]='*';
+    }
+    
+        for(i=0;b[i]!='\0';i++)
+    {
+        if((b[i]>='a'&&b[i]<='z') || (b[i]>='A'&&b[i]<='Z'))
+            if(!(b[i]=='a'||b[i]=='e'||b[i]=='i'||b[i]=='o'||b[i]=='u'||b[i]=='A'||b[i]=='E'||b[i]=='I'||b[i]=='O'||b[i]=='U'))
+               b[i]='@';
+    }
+
+    for(i=0;c[i]!='\0';i++)
+    {
+        if(c[i]>='a'&&c[i]<='z')
+            c[i]=c[i]-32;
+    }
+
+    cout << a << b<< c;
+    return 0;
+}
